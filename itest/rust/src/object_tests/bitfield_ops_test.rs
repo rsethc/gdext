339037e5dc6882c4f5662004e5e9a66e238a6b94
Seth Carter <rsethc@gmail.com>
2026-06-10 17:08:48 +0100

    cargo +nightly fmt

--- itest/rust/src/object_tests/bitfield_ops_test.rs
+++ itest/rust/src/object_tests/bitfield_ops_test.rs
@@ -11,7 +11,7 @@ use godot::obj::EngineBitfield;
 use crate::framework::itest;
 
 /// Necessarily since `from_ord` is not `const`.
-fn no_flags () -> DuplicateFlags {
+fn no_flags() -> DuplicateFlags {
     // Could use `Default::default()` here.
     // Until it is broken by `!1630`, funnily enough.
 
@@ -28,10 +28,7 @@ fn bitfield_ops_with() {
 
     // Test that adding a flag to nothing, with `with` function,
     // gives exactly that flag.
-    assert_eq!(
-        no_flags.with(USE_INSTANTIATION),
-        USE_INSTANTIATION
-    );
+    assert_eq!(no_flags.with(USE_INSTANTIATION), USE_INSTANTIATION);
 
     // Test adding another flag to an existing one.
     assert_eq!(
@@ -41,25 +38,16 @@ fn bitfield_ops_with() {
 
     // Test adding flags which are already present,
     // which should result in no change.
-    assert_eq!(
-        GROUPS.with(GROUPS),
-        GROUPS,
-    );
+    assert_eq!(GROUPS.with(GROUPS), GROUPS,);
 
     // Test that when adding a flag, where some are
     // already present, the ones that are supposed to
     // be added do in fact get added.
-    assert_eq!(
-        GROUPS.with(GROUPS.with(SIGNALS)),
-        GROUPS.with(SIGNALS),
-    );
+    assert_eq!(GROUPS.with(GROUPS.with(SIGNALS)), GROUPS.with(SIGNALS),);
 
     // Test that adding no flags at all is successful
     // and does not result in any change.
-    assert_eq!(
-        GROUPS.with(no_flags),
-        GROUPS
-    );
+    assert_eq!(GROUPS.with(no_flags), GROUPS);
 }
 
 #[itest]
@@ -68,25 +56,14 @@ fn bitfield_ops_without() {
 
     // Test that removing a flag from itself with `without`
     // gives no flags.
-    assert_eq!(
-        USE_INSTANTIATION.without(USE_INSTANTIATION),
-        no_flags
-    );
+    assert_eq!(USE_INSTANTIATION.without(USE_INSTANTIATION), no_flags);
 
     // Test removing a flag from several.
-    assert_eq!(
-        SIGNALS
-            .with(GROUPS)
-            .without(SIGNALS),
-        GROUPS
-    );
+    assert_eq!(SIGNALS.with(GROUPS).without(SIGNALS), GROUPS);
 
     // Test removing a flag that does not exist. The result
     // should be unchanged.
-    assert_eq!(
-        GROUPS.without(SIGNALS),
-        GROUPS
-    );
+    assert_eq!(GROUPS.without(SIGNALS), GROUPS);
 
     // Test removing several flags, where some were present
     // and others were not. The ones that were present should
@@ -100,8 +77,5 @@ fn bitfield_ops_without() {
 
     // Test that removing no flags at all is successful
     // and does not result in any change.
-    assert_eq!(
-        GROUPS.without(no_flags),
-        GROUPS
-    );
+    assert_eq!(GROUPS.without(no_flags), GROUPS);
 }
